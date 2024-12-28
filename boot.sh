ece9e485a5640bdb1501d714c455076c5e463d7e
Simon Mertins <mertins99@gmail.com>
2024-12-28 16:32:17 +0100

    commit

--- boot.sh
+++ boot.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-hello world
+sudo apt update >/dev/null
+sudo apt install -y git >/dev/null
+
+echo "Cloning files....."
+rm -rf $HOME/dev
+git clone https://github.com/mertins3650/dev.git $HOME/dev >/dev/null
+
+
