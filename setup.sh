1558e79d3f985c1cbed16c43a50ec1744d64dca3
Simon Mertins <mertins99@gmail.com>
2024-12-28 22:53:08 +0100

    commit

--- setup.sh
+++ setup.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env bash
 
+# gum is needed for selection
+cd /tmp || exit 1
+GUM_VERSION="0.14.3"
+wget -qO gum.deb "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_amd64.deb"
+sudo apt-get install -y ./gum.deb
+if ! command -v gum >/dev/null 2>&1; then
+    echo "Error: gum installation failed. Exiting."
+    exit 1
+fi
+[[ -f gum.deb ]] && rm gum.deb
+cd - || exit 1
 
-read -p "Do you want to remove  $HOME/dev and clone the repository? (y/n): " clone_choice
-if [[ "$clone_choice" != "y" ]]; then
+CHOICE1=("Yes" "No")
+answer1=$(gum choose "${CHOICE1[@]}" --height 10 --header "Clone repository?")
+
+if [[ "${answer1,,}" == "no" ]]; then
     echo "Exiting."
     exit 0
 else
     echo "Cloning files..."
-    sudo apt-get update >/dev/null
-    sudo apt-get install -y git >/dev/null
+    sudo apt-get update >/dev/null 2>&1
+    sudo apt-get install -y git >/dev/null 2>&1
     rm -rf "$HOME/dev"
     if git clone https://github.com/mertins3650/dev.git "$HOME/dev" >/dev/null; then
         echo "Clone successful!"
@@ -18,8 +31,9 @@ else
     fi
 fi
 
-read -p "Do you want to execute $HOME/dev/run? (y/n): " run_choice
-if [[ "$run_choice" == "y" ]]; then
+CHOICE2=("Yes" "No")
+answer2=$(gum choose "${CHOICE2[@]}" --height 10 --header "Install apps?")
+if [[ "$answer2" == "Yes" ]]; then
     if [[ -x "$HOME/dev/run" ]]; then
         "$HOME/dev/run"
     else
