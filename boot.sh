9bdf461b726a8173ee05c1b15564d80f750880c3
Simon Mertins <mertins99@gmail.com>
2024-12-28 17:03:42 +0100

    boot.sh fix

--- boot.sh
+++ boot.sh
@@ -1,25 +1,23 @@
 #!/usr/bin/env bash
 
-
-read -p "Do you want to remove the existing directory and clone the repository? (y/n): " clone_choice
-if [[ "$clone_choice" != "y" ]]; then
-    echo "Skipping cloning process. Exiting."
-    exit 0
-fi
-
 sudo apt update >/dev/null
 sudo apt install -y git >/dev/null
 
-echo "Cloning files..."
-rm -rf "$HOME/dev"
-if git clone https://github.com/mertins3650/dev.git "$HOME/dev" >/dev/null; then
-    echo "Clone successful!"
+read -p "Do you want to $HOME/dev and clone the repository? (y/n): " clone_choice
+if [[ "$clone_choice" != "y" ]]; then
+    echo "Exiting."
+    exit 0
 else
-    echo "Failed to clone repository. Exiting."
-    exit 1
+    echo "Cloning files..."
+    rm -rf "$HOME/dev"
+    if git clone https://github.com/mertins3650/dev.git "$HOME/dev" >/dev/null; then
+        echo "Clone successful!"
+    else
+        echo "Failed to clone repository. Exiting."
+        exit 1
+    fi
 fi
 
-# Prompt the user to execute the script
 read -p "Do you want to execute $HOME/dev/run? (y/n): " run_choice
 if [[ "$run_choice" == "y" ]]; then
     if [[ -x "$HOME/dev/run" ]]; then
