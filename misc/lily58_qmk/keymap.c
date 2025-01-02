1925aa66dac93fdd7f9802504b3ccb93a1dedf81
Simon Mertins <mertins99@gmail.com>
2025-01-02 10:43:46 +0100

    automated dev commit

--- misc/lily58_qmk/keymap.c
+++ misc/lily58_qmk/keymap.c
@@ -7,8 +7,8 @@ enum layer_number {
   _RAISE,
 };
 
-#define KC_SYM MO(_RAISE)
 #define KC_EXT MO(_LOWER)
+#define KC_SYM MO(_RAISE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -16,9 +16,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-  KC_EXT,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, LSFT(KC_1),  LSFT(KC_MINS),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RALT,
-                   KC_LALT, KC_LSFT,  KC_LGUI, LCTL_T(KC_SPC),    RALT_T(KC_ENT), KC_BSPC, KC_SYM, KC_PSCR
+  KC_RALT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, LSFT(KC_1),  LSFT(KC_MINS),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_PSCR,
+                   KC_LALT, KC_EXT,  KC_LCTL, LGUI_T(KC_SPC),    KC_ENT, KC_LSFT, KC_SYM, KC_BSPC
 ),
 
  [_LOWER] = LAYOUT(
