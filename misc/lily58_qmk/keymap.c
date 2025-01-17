6612727945fae769716f4cb391b365e069429b57
Simon Mertins <mertins99@gmail.com>
2025-01-17 14:58:42 +0100

    automated dev commit

--- misc/lily58_qmk/keymap.c
+++ misc/lily58_qmk/keymap.c
@@ -8,6 +8,14 @@ enum layer_number {
   _ADJUST,
 };
 
+enum {
+    TD_ALT_CAPS,
+};
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_ALT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_CAPS),
+};
+
 #define KC_EXT MO(_LOWER)
 #define KC_SYM MO(_RAISE)
 #define KC_ADJ MO(_ADJUST)
@@ -20,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
   KC_ADJ,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, LSFT(KC_1),  LSFT(KC_MINS),   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_PSCR,
-                    KC_LALT, KC_EXT,  KC_LCTL, LGUI_T(KC_SPC),    KC_ENT, KC_LSFT, KC_SYM, KC_BSPC
+                    TD(TD_ALT_CAPS), KC_EXT,  KC_LCTL, LGUI_T(KC_SPC),    KC_ENT, KC_LSFT, KC_SYM, KC_BSPC
 ),
 
  [_LOWER] = LAYOUT(
