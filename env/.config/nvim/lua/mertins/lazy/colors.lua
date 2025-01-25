c1f251479c57660880a4e7744c6779856769bd88
Simon Mertins <mertins99@gmail.com>
2025-01-25 19:31:30 +0100

    automated dev commit

--- env/.config/nvim/lua/mertins/lazy/colors.lua
+++ env/.config/nvim/lua/mertins/lazy/colors.lua
@@ -1,72 +1,12 @@
---[[
-return {
-	{
-		"tjdevries/colorbuddy.nvim",
-		config = function()
-			vim.cmd.colorscheme("gruvbuddy")
-		end,
-	},
-}
-]]
-
 function ColorMyPencils(color)
-	--color = color or "tokyonight-night"
-	color = color or "kanagawa"
-	--color = color or "catppuccin"
-	--color = color or "rose-pine-moon"
+	color = color or "catppuccin-frappe"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
-	{
-		"folke/tokyonight.nvim",
-		name = "tokyonight",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("tokyonight").setup({})
-			ColorMyPencils()
-		end,
-	},
-	{
-		"rebelot/kanagawa.nvim",
-		name = "kanagawa",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			---@diagnostic disable-next-line: missing-fields
-			require("kanagawa").setup({
-				colors = {
-					theme = {
-						all = {
-							ui = {
-								bg_gutter = "none",
-							},
-						},
-					},
-				},
-			})
-			ColorMyPencils()
-		end,
-	},
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("rose-pine").setup({
-				styles = {
-					transparency = true,
-					italic = false,
-				},
-			})
-			ColorMyPencils()
-		end,
-	},
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
@@ -74,7 +14,6 @@ return {
 		priority = 1000,
 		config = function()
 			require("catppuccin").setup({
-				flavor = "mocha",
 				transparent_background = true,
 				no_italic = false,
 				styles = {
