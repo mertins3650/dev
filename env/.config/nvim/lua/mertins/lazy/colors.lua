18bf120e76027b717154cfb97258c145b51f2516
Simon Mertins <mertins99@gmail.com>
2025-01-25 16:14:41 +0100

    automated dev commit

--- env/.config/nvim/lua/mertins/lazy/colors.lua
+++ env/.config/nvim/lua/mertins/lazy/colors.lua
@@ -1,14 +1,18 @@
---[[return {
+--[[
+return {
 	{
 		"tjdevries/colorbuddy.nvim",
 		config = function()
 			vim.cmd.colorscheme("gruvbuddy")
 		end,
 	},
-}]]
+}
+]]
 
 function ColorMyPencils(color)
-	color = color or "catppuccin"
+	--color = color or "tokyonight-night"
+	color = color or "kanagawa"
+	--color = color or "catppuccin"
 	--color = color or "rose-pine-moon"
 	vim.cmd.colorscheme(color)
 
@@ -17,6 +21,37 @@ function ColorMyPencils(color)
 end
 
 return {
+	{
+		"folke/tokyonight.nvim",
+		name = "tokyonight",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("tokyonight").setup({})
+			ColorMyPencils()
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		name = "kanagawa",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			---@diagnostic disable-next-line: missing-fields
+			require("kanagawa").setup({
+				colors = {
+					theme = {
+						all = {
+							ui = {
+								bg_gutter = "none",
+							},
+						},
+					},
+				},
+			})
+			ColorMyPencils()
+		end,
+	},
 	{
 		"rose-pine/neovim",
 		name = "rose-pine",
