4662e1f50395545519d26deec0518fc61d5bbd5a
Simon Mertins <mertins99@gmail.com>
2025-02-19 09:32:02 +0100

    blka

--- env/.config/nvim/lua/mertins/lazy/catppuccin.lua
+++ env/.config/nvim/lua/mertins/lazy/catppuccin.lua
@@ -1,11 +1,3 @@
-function ColorMyPencils(color)
-	color = color or "catppuccin-frappe"
-	vim.cmd.colorscheme(color)
-
-	--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
 return {
 	{
 		"catppuccin/nvim",
@@ -22,7 +14,7 @@ return {
 					keywords = {},
 				},
 			})
-			ColorMyPencils()
+			vim.cmd.colorscheme("catppuccin-mocha")
 		end,
 	},
 }
