026e441017c671ed78e98142b4858374a063c786
Simon Mertins <mertins99@gmail.com>
2025-05-27 14:58:19 +0200

    runs

--- env/.config/nvim/lua/mertins/lazy/lsp.lua
+++ env/.config/nvim/lua/mertins/lazy/lsp.lua
@@ -13,6 +13,9 @@ return {
 		"neovim/nvim-lspconfig",
 		dependencies = {
 			"saghen/blink.cmp",
+			{ "mason-org/mason.nvim", opts = {} },
+			"mason-org/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
 			{ "j-hui/fidget.nvim", opts = {} },
 		},
 		config = function()
@@ -74,7 +77,6 @@ return {
 
 			-- LSP Servers Configuration
 			local servers = {
-				ts_ls = {},
 				lua_ls = {
 					settings = {
 						Lua = {
@@ -90,17 +92,28 @@ return {
 						},
 					},
 				},
-				nil_ls = {},
-				gopls = {},
-				html = {},
-				basedpyright = {},
 			}
 
-			for server, config in pairs(servers) do
-				lspconfig[server].setup(vim.tbl_deep_extend("force", {
-					capabilities = capabilities,
-				}, config))
-			end
+			local ensure_installed = vim.tbl_keys(servers or {})
+			vim.list_extend(ensure_installed, {
+				"stylua", -- Used to format Lua code
+			})
+			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+			require("mason-lspconfig").setup({
+				ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+				automatic_installation = false,
+				handlers = {
+					function(server_name)
+						local server = servers[server_name] or {}
+						-- This handles overriding only values explicitly passed
+						-- by the server configuration above. Useful when disabling
+						-- certain features of an LSP (for example, turning off formatting for ts_ls)
+						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+						require("lspconfig")[server_name].setup(server)
+					end,
+				},
+			})
 		end,
 	},
 }
