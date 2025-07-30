9ec3f9ed4ab94b9eacc9c11068b642b6af1045a4
Simon Mertins <mertins99@gmail.com>
2025-07-30 21:24:14 +0200

    update

--- misc/lily58_qmk/keymap.c
+++ misc/lily58_qmk/keymap.c
@@ -43,9 +43,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_ADJUST] = LAYOUT(
   QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  
-  TAB_R,   _______, _______, _______, KC_MUTE, KC_VOLU,                   KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______, KC_F12,
-  TAB_L,   _______, _______, _______, _______, KC_VOLD,                   KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
-  KC_LGUI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LCTL,
+  TAB_R,   _______, KC_1,    KC_2,    KC_3,    KC_VOLU,                   KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______, KC_F12,
+  TAB_L,   _______, KC_4,    KC_5,    KC_6,    KC_VOLD,                   KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
+  KC_LGUI, _______, KC_7,    KC_8,    KC_9,    KC_0   , _______, _______, _______, _______, _______, _______, _______, KC_LCTL,
                              _______, _______, _______, _______, KC_PENT, _______, _______, EE_CLR
 ),
 };
