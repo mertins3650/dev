d4e51a3ddc5bf6ffbf78a191c5aa827534670bed
Simon Mertins <simon@mertins.net>
2025-10-31 15:36:28 +0100

    remove unused

--- misc/iris_qmk/keymap.c
+++ misc/iris_qmk/keymap.c
@@ -8,12 +8,10 @@ enum layer_number {
   _QWERTY = 0,
   _LOWER,
   _ADJUST,
-  _NUM,
 };
 
 #define KC_EXT MO(_LOWER)
 #define KC_ADJ MO(_ADJUST)
-#define KC_NUM MO(_NUM)
 #define TAB_R  LCTL(KC_TAB)
 #define TAB_L  LCTL(LSFT(KC_TAB))
 
@@ -33,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q, KC_W, KC_E, KC_R,   KC_T,                              KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,    KC_LBRC,
   KC_LSFT, KC_A, KC_S, KC_D, KC_F,   KC_G,                              KC_H,    KC_J,  KC_K,    KC_L,   KC_SCLN, KC_QUOT,
   KC_LGUI, KC_Z, KC_X, KC_C, KC_V,   KC_B,    KC_LALT,         KC_BSPC, KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_PSCR,
-                             KC_ADJ, KC_LCTL, LGUI_T(KC_SPC),  LT(_NUM, KC_ENT),  KC_LSFT, KC_EXT
+                             KC_ADJ, KC_LCTL, KC_SPC,          KC_ENT,  KC_LSFT, KC_EXT
 ),
  [_LOWER] = LAYOUT(
   _______, _______,       _______,    _______,       _______,       LSFT(KC_EQL),                   KC_EQL,        _______,       _______,    _______,    _______,    QK_BOOT,
@@ -49,13 +47,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   TAB_L,   RM_HUEU, RM_SATU, RM_VALU, _______, KC_VOLD,                    KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
   KC_LGUI, RM_HUED, RM_SATD, RM_VALD, _______, _______, _______,  _______, _______, _______, _______, _______, _______, KC_LCTL,
                                       _______, _______, _______,  KC_PENT, _______, EE_CLR
-),
-[_NUM] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
-  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-  _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,
-                                      _______, _______, _______,  KC_PENT, _______, _______
 ),
 };
 
